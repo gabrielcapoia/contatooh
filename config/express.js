c699d570e46859d4280dcc2434f883b8e6520243
Gabriel Capoia <gcapoia@gmail.com>
2015-08-24 13:21:12 -0300

    Create routes and configs
    Add route and config
    Add home controller
    Add home ejs

--- config/express.js
+++ config/express.js
@@ -1,1 +1,22 @@
 var express = require('express');
+var home = require('../app/routes/home');
+
+module.exports = function (argument) {
+	var app = express();
+
+	//configuração de ambiente
+	app.set('port',3000);
+
+	//middleware
+	app.use(express.static('./public'));
+
+	home(app);
+
+	app.set('view engine','ejs');
+	app.set('views','./app/views');
+
+
+
+	return app;
+}
+
