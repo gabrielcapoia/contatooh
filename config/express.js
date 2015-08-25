92c5eb6e9615db0ac2b2c965c80883d4a3c398e7
Gabriel Capoia <gcapoia@gmail.com>
2015-08-25 13:18:11 -0300

    Add getAll and getById

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function (argument) {
 	var app = express();
@@ -9,14 +10,20 @@ module.exports = function (argument) {
 
 	//middleware
 	app.use(express.static('./public'));
+	app.set('view engine','ejs');
+	app.set('views','./app/views');
 
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+
+	//Routes
 	load('models',{cwd:'app'})
 		.then('controllers')
 		.then('routes')
 		.into(app);
 
-	app.set('view engine','ejs');
-	app.set('views','./app/views');
+	
 
 	return app;
 }
