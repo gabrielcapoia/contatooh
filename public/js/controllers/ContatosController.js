f6bdc7aeff0520b90c56605eec29d4048f051fe1
Gabriel Capoia <gcapoia@gmail.com>
2015-08-28 13:15:59 -0300

    Add filter to contatos list

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -6,4 +6,24 @@ angular.module('contatooh').controller('ContatosController', function($scope){
 		$scope.total++;
 	};
 
+	$scope.contatos = [
+		{
+			"_id": 1,
+			"nome": "Contato Angular 1",
+			"email": "cont1@empresa.com.br"
+		},
+		{
+			"_id": 2,
+			"nome": "Contato Angular 2",
+			"email": "cont2@empresa.com.br"
+		},
+		{
+			"_id": 3,
+			"nome": "Contato Angular 3",
+			"email": "cont3@empresa.com.br"
+		}
+	];
+
+	$scope.filtro = '';
+
 });
